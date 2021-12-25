93aa5147acbf66e36860cbfdefcf2036c6ed1432
Coocko <krabik4794@gmail.com>
2021-12-25 14:05:01 +0200

    test passed

--- count-the-digit.py
+++ count-the-digit.py
@@ -1,9 +1,9 @@
+
 def nb_dig(n, d):
     # your code
-    result = []
-    to_squares = range(0,n,1)
-    for i in range(len(to_squares)):
-        to_squares[i] = to_squares[i]*to_squares[i]
+    to_squares = sum([str(i**2).count(str(d)) for i in list(range(0,n+1,1))])
+
+    return to_squares
 
 
 print(nb_dig(10,1))
