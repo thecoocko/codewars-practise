996e67ca315f214f27708b6017ac903c6f3e2e47
Coocko <krabik4794@gmail.com>
2021-12-26 11:43:13 +0200

    added the best practise

--- are-the-numbres-in-order.py
+++ are-the-numbres-in-order.py
@@ -4,7 +4,8 @@ import functools
 def in_asc_order(arr):
     # random_ is not allowed
 
-    bool = functools.reduce(lambda x,y: x and y, map(lambda p,q: p==q,sorted(arr, reverse=False),arr),True)
+    bool = arr == sorted(arr)
+    #functools.reduce(lambda x,y: x and y, map(lambda p,q: p==q,sorted(arr, reverse=False),arr),True)
 
 
     return bool #(True or false)
