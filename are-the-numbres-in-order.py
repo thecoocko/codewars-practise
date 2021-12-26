4845d3d3c545aa2f5fc41b5aa3f61c88321573a7
Coocko <krabik4794@gmail.com>
2021-12-26 11:34:49 +0200

    done

---
+++ are-the-numbres-in-order.py
@@ -0,0 +1,14 @@
+import functools
+
+
+def in_asc_order(arr):
+    # random_ is not allowed
+
+    bool = functools.reduce(lambda x,y: x and y, map(lambda p,q: p==q,sorted(arr, reverse=False),arr),True)
+
+
+    return bool #(True or false)
+
+
+
+print(in_asc_order([1,2,3,4]))
